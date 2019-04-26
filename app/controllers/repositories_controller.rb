b5bd05046350527adb1fc86586c12ba30c392133
Rachel Wyatt <rw126@evansville.edu>
2019-04-26 11:34:10 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,12 @@
 class RepositoriesController < ApplicationController
 
   def search
-    Faraday.get '/search/repositories' do |req|
+    @resp = Faraday.get '/search/repositories' do |req|
       req.params['client_id'] = Iv1.6620a308191a88ee
       req.params['client_secret'] = e6decad100e334b413d07b75236a068f9f2710a3
       req.params['query'] = params[:query]
     end
+    render 'search'
   end
 
   def github_search
